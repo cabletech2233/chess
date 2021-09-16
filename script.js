4df8c1337a98968846cd266a56a136e1e10eb6db
cabletech2233 <89832435+cabletech2233@users.noreply.github.com>
2021-09-16 16:54:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ var a = 0
 var lista_boja = ["red", "brown", "gray", "purple", "blue", "green"]
 function boja_ispune() {
 
+    var r = (Math.floor(Math.random() * 256)).toString();
+    var b = (Math.floor(Math.random() * 256)).toString();
+    var g = (Math.floor(Math.random() * 256)).toString();
+    console.log(r, g, b)
     var cir = document.getElementById("c");
-    cir.setAttribute("fill", lista_boja[a])
+    cir.setAttribute("fill", "rgb(" + r + "," + g + "," + b + ")")
     a += 1;
     if (a == 6) {
         a = 0
