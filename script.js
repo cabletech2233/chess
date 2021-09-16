ec30a0ead775de5e0db33f0c212d4ea70d3c26d4
cabletech2233 <89832435+cabletech2233@users.noreply.github.com>
2021-09-16 17:00:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function boja_ispune() {
     var r = (Math.floor(Math.random() * 256)).toString();
     var b = (Math.floor(Math.random() * 256)).toString();
     var g = (Math.floor(Math.random() * 256)).toString();
-    console.log(r, g, b)
     var cir = document.getElementById("c");
     cir.setAttribute("fill", "rgb(" + r + "," + g + "," + b + ")")
     a += 1;
